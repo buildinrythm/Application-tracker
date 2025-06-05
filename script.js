4ed0c5d315b1f226ab2363e97596745ed9c7fc21
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2025-06-05 21:55:38 +0100

    Added event listenr to stop page refresh

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('job-form')
+
+form.addEventListener('submit', function(event) {
+event.preventDefault();
+
+const companyInput = document.getElementById('company')
+const companyRole = document.getElementById('role')
+const companyDate = document.getElementById('date')
+const companyStatus = document.getElementById('status')
+});
