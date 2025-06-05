ddf0ec6deba26775e9bb5c69e65f3ae22b268ab0
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2025-06-05 22:07:31 +0100

    set variables to capture input values

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ form.addEventListener('submit', function(event) {
 event.preventDefault();
 
 const companyInput = document.getElementById('company')
-const companyRole = document.getElementById('role')
-const companyDate = document.getElementById('date')
-const companyStatus = document.getElementById('status')
-});
+const roleInput = document.getElementById('role')
+const statusInput = document.getElementById('status')
+const dateInput = document.getElementById('date')
+
+const company = companyInput.value;
+const role = roleInput.value;
+const status = statusInput.value;
+const date = dateInput.value;
+
+console.log(company)
+console.log(role)
+console.log(status)
+console.log(date)
+
+});ß
