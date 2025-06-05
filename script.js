f996922d08ca851e39073934503b8483e3b81eed
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2025-06-05 23:09:40 +0100

    Created and attatched cells to the table

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('job-form')
-
+const table = document.getElementById('job-table')
+/* Stop browser reset */
 form.addEventListener('submit', function(event) {
 event.preventDefault();
 
@@ -8,6 +9,7 @@ const roleInput = document.getElementById('role')
 const statusInput = document.getElementById('status')
 const dateInput = document.getElementById('date')
 
+/* Collecting input values */
 const company = companyInput.value;
 const role = roleInput.value;
 const status = statusInput.value;
@@ -18,4 +20,10 @@ console.log(role)
 console.log(status)
 console.log(date)
 
-});ß
+const newRow = document.createElement('tr')
+const tableCell = document.createElement('td')
+
+tableCell.textContent = (company.value, role.value);
+
+console.log(tableCell)
+})
